aa74057564eb39025603949c391917bc6183ac11
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-08 16:54:35 -0700

    95. One to Many relation with migration

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,7 +9,7 @@ class Comment extends Model
 {
     use HasFactory;
 
-    public function author()
+    public function blogPost()
     {
         return $this->belongsTo('App\Models\BlogPost');
     }
