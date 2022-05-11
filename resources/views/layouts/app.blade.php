e5a3f8512e1fd73e1974c067a6d615697e96adf4
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-11 08:27:21 -0700

    117. Protecting routes (requiring authentication)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,86 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravael app - @yield("title")</title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
-    <script src="{{mix('js/app.js')}}" defer></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Test Laravel App') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="d-flex mb-3 flex-column flex-md-row align-items-center p-3 p-md-4 bg-white border-bottom shadow-lg">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Laravel app</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="{{route('home.index')}}">Home</a>
-            <a class="p-2 text-dark" href="{{route('home.contact')}}">Contact</a>
-            <a class="p-2 text-dark" href="{{route('posts.index')}}">Blog Posts</a>
-            <a class="p-2 text-dark" href="{{route('posts.create')}}">Create Post</a>
-        </nav>
-    </div>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Test Laravel App') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <div class="container">
-        @if(session('status'))
-            <div class="alert alert-success">
-                {{session('status')}}
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        <li><a class="p-2 text-dark" href="{{route('home.index')}}">Home</a></li>
+                        <li><a class="p-2 text-dark" href="{{route('home.contact')}}">Contact</a></li>
+                        <li><a class="p-2 text-dark" href="{{route('posts.index')}}">Blog Posts</a></li>
+                        <li><a class="p-2 text-dark" href="{{route('posts.create')}}">Create Post</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        @endif
-        @yield("content")
+        </nav>
+
+        <main class="py-4 container">
+            @yield('content')
+        </main>
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php /** @noinspection ALL */
 
 use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
@@ -15,95 +16,12 @@ use App\Http\Controllers\AboutController;
 |
 */
 
+Auth::routes();
+
 Route::get('/', [HomeController::class, 'home'])
     ->name('home.index');
 
 Route::get('/contact', [HomeController::class, 'contact'])
     ->name('home.contact');
 
-Route::get('/single', AboutController::class);
-
 Route::resource('posts', PostsController::class);
-
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-        'has_comments' => true
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false
-    ]
-];
-
-Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
-    return "<h1> Post #</h1>" . $daysAgo;
-})->name('posts.recent.index');
-
-
-Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
-
-    Route::get('/response', function () use ($posts) {
-        return response($posts, 201)
-            ->header('Content-Type', 'application/json')
-            ->cookie('MYCOOKIE', 'TEST', '3600');
-    })->name('response');
-
-    Route::get('/redirect', function () {
-        return redirect('/contact');
-    })->name('redirect');
-
-
-    Route::get('/back', function () {
-        return back();
-    })->name('back');
-
-
-    Route::get('/named', function () {
-        return redirect()->route('posts.show', ['id' => 2]);
-    })->name('named');
-
-    Route::get('/away', function () {
-        return redirect()->away('https://google.com');
-    })->name('away');
-
-
-    Route::get('/json', function () use ($posts) {
-        return response()->json($posts);
-    })->name('json');
-
-    Route::get('/download', function () use ($posts) {
-        return response()->download(public_path('/MVC.png'), 'test.png');
-    })->name('download');
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
