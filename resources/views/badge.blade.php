25643671091ab75c8a010db39de0a45125d9e9a5
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-12 17:12:15 -0700

    150. Blade Components introduction

---
+++ resources/views/badge.blade.php
@@ -0,0 +1,1 @@
+<div class="badge badge-{{$type ?? 'succsess'}}">{{$slot}}</div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,14 +6,16 @@
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }}</p>
     <p>Added {{$post->created_at->diffForHumans()}}</p>
-    @if(now()->diffInMinutes($post->created_at) < 5)
-    <div class="alert alert-info">New!</div>
+    @if(now()->diffInMinutes($post->created_at) < 20)
+        @component('badge', ['type' => 'primary'])
+            Brand new!
+        @endcomponent
     @endif
 
     <h4>Comments</h4>
     @forelse($post->comments as $comment)
 
-        <p class="mt-3" >
+        <p class="mt-3">
             {{$comment->content}} , <span class="text-muted">added {{$comment->created_at->diffForHumans()}}</span>
         </p>
 
