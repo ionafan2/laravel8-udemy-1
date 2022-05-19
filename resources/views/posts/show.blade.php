050ec3b347c3643616f4cd33d49c99b876769797
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-18 21:01:01 -0700

    188. Practical: Displaying uploaded image and styling

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,14 +5,23 @@
 @section('content')
     <div class="row">
         <div class="col-8">
-            <h1>
-                {{ $post->title }}
-                <x-badge hide="{{ now()->diffInMinutes($post->created_at) > 10}}">New post!</x-badge>
-            </h1>
+
+            @if($post->image)
+                <div
+                    style="background-image: url('{{$post->image->url()}}'); min-height: 400px; color: white; text-align: center; background-attachment: fixed">
+                    <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+                        {{ $post->title }}
+                        <x-badge hide="{{ now()->diffInMinutes($post->created_at) > 10}}">New post!</x-badge>
+                    </h1>
+                </div>
+            @else
+                <h1>
+                    {{ $post->title }}
+                    <x-badge hide="{{ now()->diffInMinutes($post->created_at) > 10}}">New post!</x-badge>
+                </h1>
+            @endif
 
             <p>{{ $post->content }}</p>
-
-            <img src="{{$post->image->url()}}" alt="" width="100%">
 
             <p>
                 <x-updated :date="$post->created_at" name="{{$post->user->name}}">Added</x-updated>
