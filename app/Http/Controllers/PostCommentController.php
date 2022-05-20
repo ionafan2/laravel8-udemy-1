90dafab87c97e426b03c855ab909b4967a4afe8a
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-19 19:27:11 -0700

    211. The Mailable class explained 212. Rendering e-mail content and e-mail sending

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCommentRequest;
+use App\Mail\CommentPosted;
 use App\Models\BlogPost;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
@@ -19,12 +22,17 @@ class PostCommentController extends Controller
 
     public function store(BlogPost $post, StoreCommentRequest $request)
     {
-        $post->comments()->create([
+        /** @var Comment $comment */
+        $comment = $post->comments()->create([
             'content' => $request->input('content'),
             'user_id' => $request->user()->id,
         ]);
 
         $request->session()->flash('status', 'Comment added!');
+
+        Mail::to($post->user)->send(
+            new CommentPosted($comment)
+        );
 
         return redirect()->back();
     }
