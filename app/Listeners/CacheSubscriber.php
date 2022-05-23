3d1f74f46b5c4a4e7ae5f251db01b21418d3fb77
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-22 19:57:25 -0700

    232. Handling built-in Laravel events with Subscriber

---
+++ app/Listeners/CacheSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Cache\Events\CacheHit;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\Facades\Log;
+
+class CacheSubscriber
+{
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(
+            CacheHit::class,
+            '\App\Listeners\CacheSubscriber@handleCacheHit'
+        );
+
+        $events->listen(
+            CacheMissed::class,
+            '\App\Listeners\CacheSubscriber@handleCacheMissed'
+        );
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param CacheHit $event
+     * @return void
+     */
+    public function handleCacheHit(CacheHit $event)
+    {
+        Log::info($event->key . " cache HIT");
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param CacheMissed $event
+     * @return void
+     */
+    public function handleCacheMissed(CacheMissed $event)
+    {
+        Log::info($event->key . " cache Missed");
+    }
+}
