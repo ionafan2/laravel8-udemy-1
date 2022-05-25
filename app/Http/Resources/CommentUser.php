072ecb0e8925ef81a6cb6a327373d97e6d75b207
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-24 20:40:04 -0700

    254. Conditional serialization of properties

--- app/Http/Resources/CommentUser.php
+++ app/Http/Resources/CommentUser.php
@@ -17,6 +17,7 @@ class CommentUser extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'email' => $this->when(true, $this->email)
         ];
     }
 }
