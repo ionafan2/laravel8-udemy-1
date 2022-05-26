9c1e0f16b95fca55b7db8166ff066bdee30897ab
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-05-25 21:33:06 -0700

    264. API Authorization

--- app/Http/Controllers/Api/V1/PostCommentController.php
+++ app/Http/Controllers/Api/V1/PostCommentController.php
@@ -57,6 +57,8 @@ class PostCommentController extends Controller
      */
     public function update(BlogPost $post, Comment $comment, StoreCommentRequest $request)
     {
+        $this->authorize($comment);
+
         $comment->content = $request->input('content');
         $comment->save();
 
@@ -70,6 +72,8 @@ class PostCommentController extends Controller
      */
     public function destroy(BlogPost $post, Comment $comment)
     {
+        $this->authorize($comment);
+
         $comment->delete();
 
         return response()->noContent();
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Models\BlogPost' => 'App\Policies\BlogPostPolicy',
         'App\Models\User' => 'App\Policies\UserPolicy',
+        'App\Models\Comment' => 'App\Policies\CommentPolicy',
     ];
 
     /**
